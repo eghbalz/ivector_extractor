1700a46c49bb168206015cc73ece3303eba6f740
Hamid <h.eghbalz@gmail.com>
2018-02-02 11:12:12 +0100

    Update get_ivecor.py

--- get_ivecor.py
+++ get_ivecor.py
@@ -1,11 +1,11 @@
 def get_ivecor(N,F,S,T,tv_dim,ndim ,nmix):
-  % N: 0th Baum-Welche statistics
-  % F: 1st Bawm Welche statistics
-  % S: Flattened diagonal covariance matrix of ubm
-  % T: total variability space matrix (tvs)
-  % tv_dim: dimension of tvs
-  % ndim: feature dimention
-  % nmix: number of gaussian components in ubm
+#    N: 0th Baum-Welche statistics
+#   % F: 1st Bawm Welche statistics
+#   % S: Flattened diagonal covariance matrix of ubm
+#   % T: total variability space matrix (tvs)
+#   % tv_dim: dimension of tvs
+#   % ndim: feature dimention
+#   % nmix: number of gaussian components in ubm
   I = np.eye(tv_dim);
   T_invS =  T/ S.T
   idx_sv = np.reshape(np.matlib.repmat(range(0, nmix), ndim, 1), ndim * nmix, 1)
